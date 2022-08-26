508d926ab7f9a7872cea141150c6f34f8304240a
JonWong2020 <jonathannwong50@aol.com>
2022-08-26 15:08:00 -0700

    added function to color time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,24 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
 // function to display the color background depending on past, present, or future
 
+function textBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+
+        console.log(this);
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+// Call functions
+
+textBlockColor();
